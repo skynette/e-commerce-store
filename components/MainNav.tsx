68d4d2417335bd750a3fc6a8060f3c10e5cfe9c4
Joshua <cutejosh2@gmail.com>
2024-01-20 18:52:54 +0100

    category on navbar

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -11,7 +11,7 @@ interface MainNavProps {
     data: Category[];
 }
 
-const MainNav: React.FC<MainNavProps> = ({data}) => {
+const MainNav: React.FC<MainNavProps> = ({ data }) => {
     const pathName = usePathname();
     const routes = data.map((route) => ({
         href: `category/${route.id}`,
@@ -20,22 +20,25 @@ const MainNav: React.FC<MainNavProps> = ({data}) => {
     }))
     return (
         <nav
-            className='mx-6 flex items-center space-x-4  lg:space-x-6'
+            className='px-4 sm:px-6 lg:px-8 flex items-center space-x-4 lg:space-x-6 mb-2'
         >
-            {
-                routes.map((route) => (
-                    <Link
-                        key={route.href}
-                        href={route.href}
-                        className={cn(
-                            "text font-medium transition-colors hover:text-black",
-                            route.active ? "text-black" : "text-neutral-500"
-                        )}
-                    >
-                        {route.label}
-                    </Link>
-                ))
-            }
+            <div>Categories</div>
+            <div className='flex items-center space-x-4 lg:space-x-6 overflow-x-scroll'>
+                {
+                    routes.map((route) => (
+                        <Link
+                            key={route.href}
+                            href={route.href}
+                            className={cn(
+                                "text font-medium transition-colors hover:text-black",
+                                route.active ? "text-black" : "text-neutral-500"
+                            )}
+                        >
+                            {route.label}
+                        </Link>
+                    ))
+                }
+            </div>
         </nav>
     )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,11 +12,11 @@ const Navbar = async () => {
             <Container>
                 <div className='relative px-4 sm:px-6 lg:px-8 flex h-16 items-center'>
                     <Link href="/" className="ml-4 lg:ml-0 gap-x-2">
-                        <p className='font-bold text-xl'>STORE</p>
+                        <p className='font-bold text-3xl'>STORE</p>
                     </Link>
-                    <MainNav data={categories}/>
                     <NavbarActions />
                 </div>
+                <MainNav data={categories}/>
             </Container>
         </nav>
     )
